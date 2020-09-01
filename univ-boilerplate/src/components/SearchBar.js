8dc122678d0de30f3e0eeafd23bc216ae8e9aa4d
Falon Landers <60403198+falonlanders@users.noreply.github.com>
2020-09-01 12:57:21 -0400

    created searchbar.js
    Created search bar, imported react, exported. Need to create CSS and Import.

---
+++ univ-boilerplate/src/components/SearchBar.js
@@ -0,0 +1,17 @@
+//IMPORT REACT
+import React from "react";
+
+//import SEARCH CSS ONCE IT IS MADE
+
+//SEARCH BAR FUNCTION
+function SearchBar() {
+  return (
+    <div className="searchBar">
+      <input type="text" placeholder="Search here..."></input>
+      <button type="submit">Search</button>
+    </div>
+  );
+}
+
+//EXPORT SEARACH FUNCTION
+export default SearchBar;
