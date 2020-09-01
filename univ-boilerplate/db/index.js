09a13c7ab8ab2db3eac0f9a274d02c0460050998
Chelsea <cw.19courtney@gmail.com>
2020-09-01 14:00:08 -0400

    init db and create/get func for bookmarks

--- univ-boilerplate/db/index.js
+++ univ-boilerplate/db/index.js
@@ -6,8 +6,39 @@ const client = new Client(DB_URL);
 
 // database methods
 
+//create/get links:
+async function createLink({ link_url, click_count, comment }){
+  try {
+    const {rows: [link]} = await client.query(`
+    INERT INTO links(link_url, click_count, comment)
+    VALUES($1, $2, $3)
+    ON CONFLICT (link_url) DO NOTHING
+    RETURNING *;
+    `, [link_url, click_count, comment]);
+
+    return link;
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getAllLinks(){
+  try {
+    const { rows } = await client.query(`
+    SELECT * FROM links;
+    `);
+
+    return rows;
+
+  } catch (error) {
+    console.error(error);
+  }
+}
 // export
 module.exports = {
   client,
+  createLink,
+  getAllLinks,
   // db methods
 }
--- univ-boilerplate/db/seed.js
+++ univ-boilerplate/db/seed.js
@@ -8,10 +8,32 @@ async function buildTables() {
   try {
     client.connect();
 
-    // drop tables in correct order
+// drop tables in correct order:
+await client.query(`
+DROP TABLE IF EXISTS taglinks;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS links;`);
 
-    // build tables in correct order
-
+// build tables in correct order:
+await client.query(`
+CREATE TABLE links(
+  id SERIAL PRIMARY KEY,
+  link_url VARCHAR(255) UNIQUE NOT NULL,
+  click_count INTEGER NOT NULL,
+  comment VARCHAR(255) NOT NULL,
+  date_shared DATE NOT NULL DEFAULT CURRENT_DATE
+);
+CREATE TABLE tags(
+  id SERIAL PRIMARY KEY,
+  tag_name VARCHAR(255) NOT NULL
+);
+CREATE TABLE link_tags(
+  "linkId" REFERENCES links(id),
+  "tagId" REFERENCES tags(id),
+  UNIQUE ("linkId", "tagId")
+);
+`);
+console.log("Finished Dropping and Building Tables..!");
   } catch (error) {
     throw error;
   }
@@ -20,6 +42,7 @@ async function buildTables() {
 async function populateInitialData() {
   try {
     // create useful starting data
+    
   } catch (error) {
     throw error;
   }
