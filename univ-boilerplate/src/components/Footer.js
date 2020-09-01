049131e4ea8f32962e84a814eb70d5a6ab704ecc
Falon Landers <60403198+falonlanders@users.noreply.github.com>
2020-09-01 13:41:03 -0400

    Created Footer.js
    Need to import footer CSS.

---
+++ univ-boilerplate/src/components/Footer.js
@@ -0,0 +1,16 @@
+//IMPORT REACT
+import React from "react";
+
+//IMPORT FOOTER CSS
+//HERE
+
+function Footer() {
+  return (
+  <div className="footer">
+      <span>Created by Falon & Chelsea </span><br>
+      <span>CONERS UNITE!</span>
+  </div>
+  );
+}
+
+export default Footer;
