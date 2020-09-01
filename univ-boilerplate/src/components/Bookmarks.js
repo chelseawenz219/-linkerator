3e922fd373c09add417d33ed99f2e8fb25770d05
Falon Landers <60403198+falonlanders@users.noreply.github.com>
2020-09-01 13:31:18 -0400

    Created Bookmark.js
    Need to create API Data prior to being able to fill in the information needed for functionality on this page.

---
+++ univ-boilerplate/src/components/Bookmarks.js
@@ -0,0 +1,26 @@
+//IMPORT REACT
+import React, { useState } from "react";
+
+//IMPORT API LINK DATA
+
+//CREATE NEW BOOKMARK FUNCTION
+//HAVE CANCEL OPTION
+//RETURN HTML
+
+//  return (
+//    <section>
+ //   <div>
+  //    <span>Create Bookmark</span>
+      
+ //       <form>
+  //          <label>URL</label>
+  //            <input type="text" value={USE THE URL VALUE}/>
+  //          <label>TAGS</label>
+   //           <input type="text" value={USE THE TAGS VALUE}/>
+   //         <label>NOTES</label>
+   //           <input type="text" value={USER COMMENT VALUE}/>
+    //          <button>Submit</button> <button>Cancel</button>
+  //      </form>
+
+
+export default NewBookmark;
