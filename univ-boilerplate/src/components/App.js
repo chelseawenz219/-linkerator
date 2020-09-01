8ba8b1cccfb36ac7e53ba8004646eb165b62a8cb
Falon Landers <60403198+falonlanders@users.noreply.github.com>
2020-09-01 13:50:32 -0400

    Update App.js

--- univ-boilerplate/src/components/App.js
+++ univ-boilerplate/src/components/App.js
@@ -1,13 +1,17 @@
+//IMPORT REACT
 import React, { useState, useEffect } from 'react';
 
+//NEED TO GET THE API DONE FIRST PRIOR TO IMPORTING - LEAVING DUMMY DATA FOR NOW
 import {
   getSomething
 } from '../api';
 
 const App = () => {
+  //NEED TO GET STATE NAMES ON WHAT WE WANT
   const [message, setMessage] = useState('');
 
   useEffect(() => {
+    //PUT API FUNCTION BELOW
     getSomething()
       .then(response => {
         setMessage(response.message);
@@ -17,6 +21,7 @@ const App = () => {
       });
   });
 
+  //CHANGE WEDNESDAY
   return (
     <div className="App">
       <h1>Hello, World!</h1>
